33840d0ae40654afe29e2b36bf0b5d7fb2a9ad98
KYJ <ouhouh2222@gmail.com>
2020-09-19 18:43:19 +0900

    A

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Button from "./Button";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      changeValue: 0,
+    };
+  }
+
+  render() {
+    return( 
+    <div>
+      <h1>{this.state.changeValue}</h1>
+      <Button name={`INCREMENT`} action={this._increment}
+      />
+
+      <Button name={`DECREMENT`} action={this._decrement}
+      />
+        
     </div>
-  );
+    );
+  }
+
+  _increment = () => {
+    this.setState((prevState) => {
+      return {changeValue: prevState.changeValue + 1};
+    });
+  };
+
+  _decrement = () => {
+    this.setState((prevState) => {
+      return {changeValue: prevState.changeValue - 1};
+    });
+  };
 }
+
 
 export default App;
